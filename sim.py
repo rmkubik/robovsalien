6563a72b8f3b6efb875af37fb4bf4f2b6819bdb8
Ryan Kubik <rmkubik@mix.wvu.edu>
2017-02-22 13:35:25 -0800

    Create basic random behavior between attacking and moving

--- sim.py
+++ sim.py
@@ -1,7 +1,26 @@
 import json
 import post
 import world as ent
+import random
+
+def moveAttackEntities(entities):
+    for entity in entities:
+        attackAndMove = random.randint(1, 100)
+        if attackAndMove <= 30:
+            # attack & move
+            entity.move()
+            entity.attack()
+        else:
+            attackOrMove = random.randint(1, 100)
+            if attackOrMove <= 60:
+                # attack
+                entity.attack()
+            else:
+                # move
+                entity.move()
+
 def update(world):
+    random.seed(world["seed"])
     robots = []
     aliens = []
 
@@ -10,6 +29,10 @@ def update(world):
 
     for alien in world["aliens"]:
         aliens.append(ent.Entity(alien["row"], alien["col"], "alien"))
+
+    moveAttackEntities(robots)
+    moveAttackEntities(aliens)
+
 twitter = post.Twitter()
 
 with open("auth.json", "r") as authFile:
