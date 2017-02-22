b2bf4b17fa31ec91e049ae91a7f7797e12a22f49
Ryan Kubik <rmkubik@mix.wvu.edu>
2017-02-22 13:33:55 -0800

    Use Entity class and new data structure to represent sim world

--- sim.py
+++ sim.py
@@ -1,5 +1,15 @@
 import json
 import post
+import world as ent
+def update(world):
+    robots = []
+    aliens = []
+
+    for robot in world["robots"]:
+        robots.append(ent.Entity(robot["row"], robot["col"], "robot"))
+
+    for alien in world["aliens"]:
+        aliens.append(ent.Entity(alien["row"], alien["col"], "alien"))
 twitter = post.Twitter()
 
 with open("auth.json", "r") as authFile:
@@ -8,4 +18,5 @@ with open("auth.json", "r") as authFile:
 
 with open("world.json", "r") as worldFile:
     world = json.loads(worldFile.read())
+    update(world)
     # twitter.post(world)
---
+++ world.py
@@ -0,0 +1,11 @@
+class Entity:
+    def __init__(self, _row, _col, _team):
+        row = _row
+        col = _col
+        team = _team
+
+    def move(self):
+        print "move"
+
+    def attack(self):
+        print "attack"
