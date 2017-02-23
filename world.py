4d5537bfdb6478fb8469ad250e1f9dd9a763a2eb
Ryan Kubik <rmkubik@mix.wvu.edu>
2017-02-23 00:28:46 -0800

    Create unicode tiles class

--- world.py
+++ world.py
@@ -9,3 +9,13 @@ class Entity:
 
     def attack(self):
         print "attack"
+class Tiles:
+    Explosion = u'\U0001F4A5'
+    Fire = u'\U0001F525'
+    Tree_Pine = u'\U0001F332'
+    Tree_Oak = u'\U0001F333'
+    Tree_Holiday = u'\U0001F384'
+    Empty = u'\u25AB'u'\uFE0F'
+    Burned = u'\u25AA'u'\uFE0F'
+    Robot = u'\U0001F916'
+    Alien = u'\U0001F47D'
